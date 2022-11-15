2716279df58e263cbbb2efc56b279151007f2b3d
nethawkc@msn.com <nethawkc@msn.com>
2022-11-15 23:17:22 +0800

    0.1

---
+++ READMD.MD
@@ -0,0 +1,6 @@
+# hardware pwm 呼吸燈
+
+使用 Raspberry pi 上的硬體 PWM 必須要做一些設定 
+
+參考  
+[Enabling Hardware PWM on Raspberry Pi](https://github.com/dotnet/iot/blob/main/Documentation/raspi-pwm.md)
---
+++ src/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Description:
+ *    使用 Raspberry pi 硬體 PWM 做出呼吸燈
+*/
+using System;
+using System.Device.Gpio;
+using System.Device.Pwm;
+using System.Threading;
+
+Console.WriteLine("Hardware PWM Breathe Led Start.");
+// chip 0, channel 0 -> GPIO 12
+int pin = 0;
+int channel = 0;
+var pwm = PwmChannel.Create(pin, channel, 400, 1.0);
+pwm.Start();
+
+while(true){
+  Console.WriteLine("逐漸變亮");
+  for (double fill = 0.0; fill <= 1.0;fill+=0.01){
+    pwm.DutyCycle = fill;
+    Thread.Sleep(10);
+  }
+  Thread.Sleep(1000);
+  Console.WriteLine("逐漸變暗");
+  for (double fill = 1.0; fill >= 0.0;fill-=0.01){
+    pwm.DutyCycle = fill;
+    Thread.Sleep(10);
+  }
+}
+
